16bd316f1aa844b3e7bf768ed269e42a24fcc295
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2022-05-28 01:18:32 +0200

    arrrggghhhhh fixed!

---
+++ tests/Acceptance/Controller/AuthTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Acceptance\Controller;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AuthTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+    }
+
+    public function testAuthRegister(): void
+    {
+        $header = [
+            'CONTENT_TYPE' => 'application/json',
+        ];
+
+        $content = '{"email":"ninja@sec//ret.com"}';
+
+        $this->client->request('POST', '/auth/register', [], [], $header, $content);
+
+        self::assertResponseStatusCodeSame(200);
+
+        $response = $this->client->getResponse();
+
+        self::assertTrue($response->headers->contains('Content-Type', 'application/json'));
+
+        $responseRequest = json_decode($response->getContent(), true);
+
+        self::assertSame(
+            'Please copy the token. After leaving the page, copying again is not possible.',
+            $responseRequest['message']
+        );
+        self::assertSame('ninja@secret.com', $responseRequest['data']['email']);
+        self::assertSame(64, strlen($responseRequest['data']['token']));
+        self::assertTrue($responseRequest['success']);
+    }
+
+    public function testAuthRegisterWithInccorectEmail(): void
+    {
+        $header = [
+            'CONTENT_TYPE' => 'application/json',
+        ];
+
+        $content = '{"email":"ninja@secret"}';
+
+        $this->client->request('POST', '/auth/register', [], [], $header, $content);
+
+        self::assertResponseStatusCodeSame(400);
+
+        $response = $this->client->getResponse();
+
+        self::assertTrue($response->headers->contains('Content-Type', 'application/json'));
+
+        $responseRequest = json_decode($response->getContent(), true);
+
+        self::assertSame('Email "ninja@secret" is not valid!', $responseRequest['message']);
+        self::assertEmpty($responseRequest['data']);
+        self::assertFalse($responseRequest['success']);
+    }
+
+}
